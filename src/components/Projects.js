11f466496a4e93516f007bd82bba12a6110ac4fc
Akbar <shaikakbarbasha38@gmail.com>
2022-03-20 23:16:19 +0530

    small changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,25 +9,26 @@ const Projects = () => {
         <span className="title">Projects</span>
       </h1>
       <Grid>
-        <Card
-          img={require('../images/medplusmart.png')}
-          title="Medplusmart Clone "
-          description="It's a clone of MedPlusmart,In which user can buy prescription medicines, OTC products & household needs.This project was done by team of 5."
-          technologies="Javascript - HTML - CSS "
-          link_two="https://medplusmartclone.000webhostapp.com/"
-          link_three="https://github.com/Akbar660/medplusmart.com"
-          link_two_text="Visit"
-          link_three_text="Github"
-        />
+       
         <Card
           img={require('../images/Zarahome.png')}
           title="Zarahome Clone"
-          description="It's a clone of Zara Home,In which user can buy styles in bedding, fragrances, tableware, rugs, lamps, duvet covers, towels and home accessories.This project was done by team of 4."
+          description="It's a clone of Zara Home,In which user can buy styles in bedding, fragrances, lamps, duvet covers and home accessories.This project was done by team of 4."
           technologies="HTML - CSS - Javascript"
       
           link_two="https://zarahome-ww-clone.netlify.app/"
           link_three="https://github.com/Akbar660/Clone-ZaraHome"
          
+          link_two_text="Visit"
+          link_three_text="Github"
+        />
+         <Card
+          img={require('../images/coolecommerce.png')}
+          title="Cool Ecommerce"
+          description="It is an Ecommerce website where we can buy different types of users related goods like furniture,electronics,as well as fashion.This project was done by team of 2."
+          technologies="Reactjs - Redux - firebase "
+          link_two="https://coolecommerce-akbar660.netlify.app/"
+          link_three="https://github.com/Akbar660/CoolEcommerce"
           link_two_text="Visit"
           link_three_text="Github"
         />
@@ -45,7 +46,7 @@ const Projects = () => {
         <Card
           img={require('../images/weatherapp.png')}
           title="Weather App"
-          description="It is a Weather App where we find weather of different cities in world."
+          description="It is a Weather App where we find weather data of different cities in world."
           technologies="Reactjs - Styled Components - Material UI "
           technologies2=""
           link_two="https://weather-app-am7jj5bj9-akbar660.vercel.app/"
